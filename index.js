a4e881a9b345c0260e655128ca5e91c278666740
vikaba <vikaba@bergen.org>
2024-04-23 09:40:23 -0400

    imgur upload

--- index.js
+++ index.js
@@ -20,19 +20,19 @@ faceapi.nets.faceRecognitionNet.loadFromDisk(weightsLoc)
     .then(() => faceapi.nets.ssdMobilenetv1.loadFromDisk(weightsLoc))
     .then(() => faceapi.nets.faceLandmark68Net.loadFromDisk(weightsLoc))
     .then(async () => {
-	console.log("got past model loading")
-	const descriptors = [];
-        for (let i = 1; i < 27; i += 1) {
-	    console.log("loading image" + i)
-            const image = fs.readFileSync(`training-images/image${i}.jpg`)
-            const queryImage1 = tf.node.decodeImage(image);
-            descriptors.push(
-                (await faceapi
-                    .detectSingleFace(queryImage1)
-                    .withFaceLandmarks()
-                    .withFaceDescriptor()).descriptor
-            );
-        }
+        console.log("got past model loading")
+        const descriptors = [];
+            for (let i = 1; i < 27; i += 1) {
+            console.log("loading image" + i)
+                const image = fs.readFileSync(`training-images/image${i}.jpg`)
+                const queryImage1 = tf.node.decodeImage(image);
+                descriptors.push(
+                    (await faceapi
+                        .detectSingleFace(queryImage1)
+                        .withFaceLandmarks()
+                        .withFaceDescriptor()).descriptor
+                );
+            }
 
         return [
             new faceapi.LabeledFaceDescriptors(personLabel, descriptors)
@@ -43,7 +43,7 @@ faceapi.nets.faceRecognitionNet.loadFromDisk(weightsLoc)
         while (true) {
             await setTimeout(timeInterval);
             const fileName = `captured-${Date.now()}.jpg`;
-	    console.log("taking a pic");	
+	        console.log("taking a pic");	
             await bluebird.fromCallback(done => {
                 exec(`python3 camera_capture.py -filename ${fileName}`, done);
             })
@@ -56,18 +56,34 @@ faceapi.nets.faceRecognitionNet.loadFromDisk(weightsLoc)
                     .withFaceLandmarks()
                     .withFaceDescriptor()
             })
-            .then(incomingFace => {
-		if (!incomingFace) {
-		    console.log("there's no one there!");
-		    return;
-		}
+            .then(async incomingFace => {
+                if (!incomingFace) {
+                    console.log("there's no one there!");
+                    return;
+                }
                 const bestMatch = faceMatcher.findBestMatch(incomingFace.descriptor)
                 if (bestMatch?._label !== personLabel) {
                     console.log("NOT YOU! INTRUDER! SUSPENDING YOUR ACCESS!");
-                    return axios.post(invokeUrl, {});
+                    const axiosImgur = axios.create({
+                        baseURL: "https://api.imgur.com/3/image",
+                        headers: {'Authorization': `Client-ID ${process.env.IMGUR_CLIENT_ID}`}
+                    });
+                    const form = new FormData();
+                    form.append("image", `@"${fileName}"`);
+                    form.append("type", "image");
+
+                    return axiosImgur.post(imgurUrl, form)
+                        .then(imgur => {
+                            console.log(imgur)
+                            const link = imgur.data.link;
+                            console.log("deletehash: ", imgur.data.deletehash);
+                            return axios.post(invokeUrl, {
+                                imageLink: link 
+                            });
+                        });
                 }
 
-		console.log("all good, it's just me!");
+                console.log("all good, it's just me!");
             });
         }
     });
