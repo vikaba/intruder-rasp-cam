018214d036ab526c32be87e1f1de05595925ac4c
vikaba <vikaba@bergen.org>
2024-04-23 09:40:23 -0400

    imgur upload

--- index.js
+++ index.js
@@ -6,30 +6,34 @@ const { exec } = require("child_process");
 const bluebird = require("bluebird");
 const { setTimeout } = require("node:timers/promises");
 const axios = require("axios");
+const {fileFromPath} = require('formdata-node/file-from-path');
 
 const timeInterval = 10 * 1000;
 const invokeUrl = process.env.INVOKE_API;
 const personLabel = process.env.PERSON_NAME || "person1";
+const weightsLoc = "face-api.js/weights"; 
 
 if (!invokeUrl) {
     throw new Error("No url to invoke");
 }
 
-faceapi.nets.faceRecognitionNet.loadFromDisk('models')
-    .then(() => faceapi.nets.ssdMobilenetv1.loadFromDisk('models'))
-    .then(() => faceapi.nets.faceLandmark68Net.loadFromDisk('models'))
+faceapi.nets.faceRecognitionNet.loadFromDisk(weightsLoc)
+    .then(() => faceapi.nets.ssdMobilenetv1.loadFromDisk(weightsLoc))
+    .then(() => faceapi.nets.faceLandmark68Net.loadFromDisk(weightsLoc))
     .then(async () => {
+        console.log("got past model loading")
         const descriptors = [];
-        for (let i = 1; i < 4; i += 1) {
-            const image = fs.readFileSync(`training_images/image${i}.jpg`)
-            const queryImage1 = tf.node.decodeImage(image);
-            descriptors.push(
-                (await faceapi
-                    .detectSingleFace(queryImage1)
-                    .withFaceLandmarks()
-                    .withFaceDescriptor()).descriptor
-            );
-        }
+            for (let i = 1; i < 27; i += 1) {
+            console.log("loading image" + i)
+                const image = fs.readFileSync(`training-images/image${i}.jpg`)
+                const queryImage1 = tf.node.decodeImage(image);
+                descriptors.push(
+                    (await faceapi
+                        .detectSingleFace(queryImage1)
+                        .withFaceLandmarks()
+                        .withFaceDescriptor()).descriptor
+                );
+            }
 
         return [
             new faceapi.LabeledFaceDescriptors(personLabel, descriptors)
@@ -40,7 +44,7 @@ faceapi.nets.faceRecognitionNet.loadFromDisk('models')
         while (true) {
             await setTimeout(timeInterval);
             const fileName = `captured-${Date.now()}.jpg`;
-
+	        console.log("taking a pic");	
             await bluebird.fromCallback(done => {
                 exec(`python3 camera_capture.py -filename ${fileName}`, done);
             })
@@ -53,12 +57,34 @@ faceapi.nets.faceRecognitionNet.loadFromDisk('models')
                     .withFaceLandmarks()
                     .withFaceDescriptor()
             })
-            .then(incomingFace => {
+            .then(async incomingFace => {
+                if (!incomingFace) {
+                    console.log("there's no one there!");
+                    return;
+                }
                 const bestMatch = faceMatcher.findBestMatch(incomingFace.descriptor)
                 if (bestMatch?._label !== personLabel) {
-                    console.log("NOT YOU! INTRUDER!!!!!");
-                    return axios.post(invokeUrl, {});
+                    console.log("NOT YOU! INTRUDER! SUSPENDING YOUR ACCESS!");
+                    const axiosImgur = axios.create({
+                        baseURL: "https://api.imgur.com/3/image",
+                        headers: {'Authorization': `Client-ID ${process.env.IMGUR_CLIENT_ID}`}
+                    });
+                    const form = new FormData();
+                    form.append("image", `@"${fileName}"`);
+                    form.append("type", "image");
+
+                    return axios.post(imgurUrl, form)
+                        .then(imgur => {
+                            console.log(imgur)
+                            const link = imgur.data.link;
+                            console.log("deletehash: ", imgur.data.deletehash);
+                            return axios.post(invokeUrl, {
+                                imageLink: link 
+                            });
+                        });
                 }
+
+                console.log("all good, it's just me!");
             });
         }
     });
